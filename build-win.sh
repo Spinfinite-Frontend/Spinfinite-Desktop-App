9019af5e401f050fcb94042b798b32863cc0c7d4
Anthony Robert <99475715+AnthonyRobert377@users.noreply.github.com>
2025-06-12 18:36:34 +0100

    f

--- build-win.sh
+++ build-win.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-electron-packager . Spinfinite \
-  --platform=win32 \
-  --arch=x64 \
-  --icon=icon.ico \
-  --app-version=1.0.3 \
-  --build-version=1.0.3 \
-  --executable-name="Spinfinite" \
-  --win32metadata.ProductName="Spinfinite Desktop" \
-  --win32metadata.CompanyName="Spinfinite Ltd" \
-  --win32metadata.FileDescription="Official Spinfinite Desktop App" \
-  --win32metadata.InternalName="SpinfiniteApp" \
-  --win32metadata.OriginalFilename="Spinfinite.exe" \
-  --out=dist \
-  --overwrite
+# Load .env file
+source .env
+
+# Run the builder with GitHub token available
+GH_TOKEN=$GH_TOKEN arch -x86_64 npx electron-builder --win --x64 --ia32 --publish always
